a0e1503a68f73c36d1fb944b3d26d54f5d3fcd53
tstadler <ths.stadler@gmail.com>
2016-05-06 13:50:27 +0200

    update addEntry

--- ageuler_rgcEphys_test3.py
+++ ageuler_rgcEphys_test3.py
@@ -1340,26 +1340,27 @@ def addEntry(animal_id,sex,date_of_birth,exp_date,eye,cell_id,data_folder,filena
                    'morphology': morph, 'type': cell_type})
         except Exception as e3:
             print(e3)
-    try:
-        if 'BWNoise' in filename:
-            rec_type = str(input('Recording type? (extracell/intracell): '))
-            ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
-            ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
-            R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'bw_noise','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
-        if 'Chirp' in filename:
-            rec_type = str(input('Recording type? (extracell/intracell): '))
-            ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
-            ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
-            R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'chirp','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
-        if 'DS' in filename:
-            rec_type = str(input('Recording type? (extracell/intracell): '))
-            ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
-            ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
-            R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'ds','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
-        if 'ON' in filename:
-            rec_type = str(input('Recording type? (extracell/intracell): '))
-            ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
-            ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
-            R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'on_off','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
-    except Exception as e4:
-        print(e4)
+    if (len(R & dict(animal_id = animal_id,exp_date = exp_date, cell_id=cell_id,filename = filename)) == 0):
+        try:
+            if 'BWNoise' in filename:
+                rec_type = str(input('Recording type? (extracell/intracell): '))
+                ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
+                ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
+                R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'bw_noise','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
+            if 'Chirp' in filename:
+                rec_type = str(input('Recording type? (extracell/intracell): '))
+                ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
+                ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
+                R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'chirp','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
+            if 'DS' in filename:
+                rec_type = str(input('Recording type? (extracell/intracell): '))
+                ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
+                ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
+                R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'ds','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
+            if 'ON' in filename:
+                rec_type = str(input('Recording type? (extracell/intracell): '))
+                ch_voltage = str(input('Name of the recording channel containing the voltage signal? '))
+                ch_trigger = str(input('Name of the recording channel containing the trigger signal? '))
+                R.insert1({'animal_id':animal_id,'exp_date':exp_date,'eye':eye,'cell_id':cell_id,'filename':filename,'stim_type':'on_off','rec_type':rec_type,'ch_voltage':ch_voltage,'ch_trigger':ch_trigger})
+        except Exception as e4:
+            print(e4)
